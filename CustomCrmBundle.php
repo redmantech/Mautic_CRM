cebcbdb1fbcfa8a188a8ddebdaf232b843545af4
danionut90 <ionut90ro@gmail.com>
2016-03-13 23:24:43 +0200

    avoiding creation of existing tables on install

--- CustomCrmBundle.php
+++ CustomCrmBundle.php
@@ -16,6 +16,13 @@ class CustomCrmBundle extends PluginBundleBase
     static public function onPluginInstall(Plugin $plugin, MauticFactory $factory, $metadata = null)
     {
         if ($metadata !== null) {
+            foreach ($metadata as $key => $entity) {
+                /** @var \Doctrine\ORM\Mapping\ClassMetadata $entity */
+                if ($factory->getDatabase()->getSchemaManager()->tablesExist($entity->getTableName())) {
+                    unset($metadata[$key]);
+                }
+            }
+
             self::installPluginSchema($metadata, $factory);
         }
 
